6de66f9f969f239a8b93ff23a66ff0b9f49c4fda
Aria Taylor <ari@willing.com>
2023-02-28 19:09:01 -0500

    Add inject_file_contents

--- zygoat/utils/__init__.py
+++ zygoat/utils/__init__.py
@@ -1,1 +1,2 @@
 from .use_dir import use_dir  # noqa
+from .inject_file_contents import inject_file_contents  # noqa
---
+++ zygoat/utils/inject_file_contents.py
@@ -0,0 +1,28 @@
+from typing import Union
+import os
+
+
+def inject_file_contents(
+    name: Union[str, os.PathLike],
+    contents: Union[str, bytes],
+    overwrite: bool = False,
+):
+    """
+    Directly writes the contents of a file. Useful for data not easily manipulated via Python, but shouldn't be the first resort for configuration changes.
+
+    :param name: Path to the file to write
+    :param contents: Data to write to the file,
+    :param overwrite: Overwrite existing files
+    """
+
+    if os.path.isfile(name) and not overwrite:
+        raise FileExistsError(
+            f"{name} already exists in the filesystem and overwrite is not specified"
+        )
+
+    # Normalize content type to bytes
+    if type(contents) is str:
+        contents = contents.encode()
+
+    with open(name, "wb") as f:
+        f.write(contents)
