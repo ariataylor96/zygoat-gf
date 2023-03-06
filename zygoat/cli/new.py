a99474b081cb953bbdc6670f99c17216669fd41a
Aria Taylor <ari@willing.com>
2023-03-06 17:37:57 -0500

    Dynamically load plan from CLI options

--- zygoat/cli/new.py
+++ zygoat/cli/new.py
@@ -1,20 +1,42 @@
 import os
 import click
+from importlib import import_module
+from typing import Callable
 
 from loguru import logger as log
 
-from zygoat.plans import backend, frontend
 from zygoat.utils import use_dir
-
-modules = [backend, frontend]
 
 
 @click.argument("name")
-def new(name):
+@click.option(
+    "--plan",
+    default="zygoat.plans:default",
+    help="Import path to the default plan module to execute - e.g. zygoat.plans:default",
+)
+def new(name, plan):
     log.info(f"Creating {name}")
     os.makedirs(name)
 
-    # TODO: load these at runtime with importlib
+    plan = _get_plan(plan)
+
     with use_dir(name):
-        for module in modules:
-            module.entrypoint()
+        plan(name=name)
+
+
+def _get_plan(import_path: str) -> Callable:
+    module, attr = import_path.split(":")
+    imported = getattr(import_module(module), attr)
+
+    if callable(imported):
+        return imported
+
+    log.info(f"{import_path} is not callable, checking for .entrypoint()")
+    imported = getattr(imported, "entrypoint")
+
+    if imported is None or not callable(imported):
+        raise ImportError(
+            f"Neither {import_path} nor {import_path}.entrypoint are callable functions, exiting"
+        )
+
+    return imported
